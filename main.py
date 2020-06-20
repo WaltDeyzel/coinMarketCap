06708a231db05a4be02a96c0946e44203fff47e2
21750793 <21750793@sun.ac.za>
2020-06-20 17:14:25 +0200

    Displaying top N best and worst Crypto

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     row = body.find_all('tr', {'class': 'cmc-table-row'})
 
     data = []
-    c = 0
+
     for coin in row:
         item = coinData()
         no = coin.find('td', {'class':"cmc-table__cell cmc-table__cell--sticky cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__rank"}).find('div').text
@@ -32,25 +32,19 @@ if __name__ == "__main__":
         name = nameLink.text
         link = nameLink.get('href')
         price = coin.find('td', {'class':"cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price"}).find('a').text
+        marketcap = coin.find('td', {'class': "cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__market-cap"}).text
         change1h = coin.find('td', {'class':"cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__percent-change-1-h"}).find('div').text
         change24h = coin.find('td', {'class':"cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__percent-change-24-h"}).find('div').text
         change7d = coin.find('td', {'class':"cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__percent-change-7-d"}).find('div').text
+        
         item.no = int(no)
         item.name = str(name)
         item.price = float(price[1:].replace(',','')) # removing comma and dollar sign
+        item.marketcap = marketcap
         item.change1h = float(change1h.replace('%', ''))
         item.change24h= float(change24h.replace('%', ''))
         item.change7d = float(change7d.replace('%', ''))
         item.link = site + link[1:]
-        if(c>0):
-            page2 = requests.get(item.link)
-            soup2 = BeautifulSoup(page2.content, 'html.parser')
-            ##print(page2)
-            body2 = soup2.find('tbody', {'class': "cmc-details-panel-about__table"})
-            roi = body2.find('span', {'class': "cmc--change-positive"}).text
-            #change positive somehow... very slow method. Move to class....
-            item.roi = (roi.replace('%','')).replace(',','')
-            c -=1
         data.append(item)
 
     # for item in data:
@@ -58,41 +52,49 @@ if __name__ == "__main__":
     #     if(item.change1h > 30.0):
     #         print(item.name)
     #         webbrowser.open(item.link)
+
+    #SORTED DATA ACCORDING TO...
     hourly = sorted(data, key=operator.attrgetter('change1h'))
     daily = sorted(data, key=operator.attrgetter('change24h'))
     weekly = sorted(data, key=operator.attrgetter('change7d'))
-    bestPerforming = [hourly, daily, weekly]
+
+    hourly_worst = hourly[:11]
+    hourly_best = hourly[-10:]
+    daily_worst = daily[:11]
+    daily_best = daily[-10:]
+    weekly_worst = weekly[:11]
+    weekly_best = weekly[-10:]
+ 
+    bestPerforming = [hourly_best, hourly_worst, daily_best, daily_worst, weekly_best, weekly_worst]
 
     for interval in bestPerforming:
         fmt = '{:<3} {:<20} {:<10} {:<10} {:<15} {:<15}'
         print(fmt.format('NO','COIN', 'PRICE', 'CHANGE', 'MARKETCAP', 'ROI'))
         print('-------------------------------------------------------------------')
-        for i in range(10):
-            coin = interval[i]
-            page2 = requests.get(coin.link)
-            soup2 = BeautifulSoup(page2.content, 'html.parser')
-            #print(page2)
-            body2 = soup2.find('tbody', {'class': "cmc-details-panel-about__table"})
-            try:
-                marketcap = body2.find_all('td')
-                coin.marketcap = (marketcap[3].text).replace(',',' ').replace('USD','')
-            except:
-                pass
-            try:
-                coin.roi = (body2.find('span', {'class': "cmc--change-positive"}).text).replace('USD','')
-            except:
-                pass
+        for coin in interval:
+            # #coin = interval[i]
+            # page2 = requests.get(coin.link)
+            # soup2 = BeautifulSoup(page2.content, 'html.parser')
+            # #print(page2)
+            # body2 = soup2.find('tbody', {'class': "cmc-details-panel-about__table"})
+            # try:
+            #     marketcap = body2.find_all('td')
+            #     coin.marketcap = (marketcap[3].text).replace(',',' ').replace('USD','')
+            # except:
+            #     pass
+            # try:
+            #     coin.roi = (body2.find('span', {'class': "cmc--change-positive"}).text).replace('USD','')
+            # except:
+            #     pass
              
             
-            if(bestPerforming[0] == interval):
+            if(bestPerforming[0] == interval or bestPerforming[1] == interval):
                 
                 print(fmt.format(coin.no, coin.name, coin.price, coin.change1h, coin.marketcap, coin.roi))
-            if(bestPerforming[1] == interval):
+            if(bestPerforming[2] == interval or bestPerforming[3] == interval):
                 
                 print(fmt.format(coin.no, coin.name, coin.price, coin.change24h, coin.marketcap, coin.roi))
-            if(bestPerforming[2] == interval):
-                
+            if(bestPerforming[4] == interval or bestPerforming[5] == interval):
+
                 print(fmt.format(coin.no, coin.name, coin.price, coin.change7d, coin.marketcap, coin.roi))
         print()
-
-
