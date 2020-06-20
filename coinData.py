fcbe7d7a7c4fb586a7c559981267bc1b4c4138ac
21750793 <21750793@sun.ac.za>
2020-06-20 21:01:46 +0200

    Changed the way data is layed out when displayed. Print next to each other instead of underneath. Also included user crypto wallet

--- coinData.py
+++
@@ -1,9 +0,0 @@
-class coinData:
-    def data(self, no, name, price, c1h, c24h, c7d):
-        self.name = name
-        self.no = no
-        self.price = price
-        self.c1h = c1h
-        self.c24h = c24h
-        self.c7d = c7d
-        print(self.name)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import operator
 class coinData:
     no = 0
     name = ''
+    tag = ''
     price = 0.0
     change1h = 0.0
     change24h = 0.0
@@ -15,21 +16,28 @@ class coinData:
     marketcap = ''
 
 if __name__ == "__main__":
+    #WEBSITE
     site = "https://coinmarketcap.com/"
     addon = 'all/views/all/'
     page = requests.get(site+addon)
 
     soup = BeautifulSoup(page.content, 'html.parser')
+    #BODY CONTAINS ALL THE DATA IN HTML.
     body = soup.find('tbody')
     row = body.find_all('tr', {'class': 'cmc-table-row'})
 
+    #STORE ALL THE CRUPTO DATA IN DATA LIST
     data = []
+    #SPESIFIC COINS 
+    wallet = ['GNT', 'ETH', 'ADA', 'CVC', 'OMG', 'ZEC', 'XRP', 'LTC', 'LSK', 'NEO', 'XMR', 'QTUM'] 
+    wallet_now = []
 
     for coin in row:
         item = coinData()
         no = coin.find('td', {'class':"cmc-table__cell cmc-table__cell--sticky cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__rank"}).find('div').text
         nameLink = coin.find('td', {'class':"cmc-table__cell cmc-table__cell--sticky cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__name"}).find('div').find('a')
         name = nameLink.text
+        tag = coin.find('td', {'class': "cmc-table__cell cmc-table__cell--sortable cmc-table__cell--left cmc-table__cell--sort-by__symbol"}).text
         link = nameLink.get('href')
         price = coin.find('td', {'class':"cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__price"}).find('a').text
         marketcap = coin.find('td', {'class': "cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__market-cap"}).text
@@ -47,54 +55,64 @@ if __name__ == "__main__":
         item.link = site + link[1:]
         data.append(item)
 
-    # for item in data:
-    #     print(item.roi)
-    #     if(item.change1h > 30.0):
-    #         print(item.name)
-    #         webbrowser.open(item.link)
+        #Add spesific coind to seprate list
+        if tag in wallet:
+            wallet_now.append(item)
+            #print(tag)
+            #webbrowser.open(item.link)
 
     #SORTED DATA ACCORDING TO...
     hourly = sorted(data, key=operator.attrgetter('change1h'))
     daily = sorted(data, key=operator.attrgetter('change24h'))
     weekly = sorted(data, key=operator.attrgetter('change7d'))
 
+    #DISPLAY TOP 10
     hourly_worst = hourly[:11]
     hourly_best = hourly[-10:]
     daily_worst = daily[:11]
     daily_best = daily[-10:]
     weekly_worst = weekly[:11]
     weekly_best = weekly[-10:]
- 
-    bestPerforming = [hourly_best, hourly_worst, daily_best, daily_worst, weekly_best, weekly_worst]
 
-    for interval in bestPerforming:
-        fmt = '{:<3} {:<20} {:<10} {:<10} {:<15} {:<15}'
-        print(fmt.format('NO','COIN', 'PRICE', 'CHANGE', 'MARKETCAP', 'ROI'))
-        print('-------------------------------------------------------------------')
-        for coin in interval:
-            # #coin = interval[i]
-            # page2 = requests.get(coin.link)
-            # soup2 = BeautifulSoup(page2.content, 'html.parser')
-            # #print(page2)
-            # body2 = soup2.find('tbody', {'class': "cmc-details-panel-about__table"})
-            # try:
-            #     marketcap = body2.find_all('td')
-            #     coin.marketcap = (marketcap[3].text).replace(',',' ').replace('USD','')
-            # except:
-            #     pass
-            # try:
-            #     coin.roi = (body2.find('span', {'class': "cmc--change-positive"}).text).replace('USD','')
-            # except:
-            #     pass
-             
+    hourly_best.reverse()
+    weekly_best.reverse()
+    daily_best.reverse()
+    #sections hourly, daily, weekly
+    bestPerforming = [[hourly_best, hourly_worst], [daily_best, daily_worst], [weekly_best, weekly_worst]]
+
+    fmt = '{:<3} {:<20} {:<10} {:<10} {:<15} {:<15}'
+    for section in bestPerforming:
+        heading = fmt.format('NO','COIN', 'PRICE', 'CHANGE', 'MARKETCAP', 'ROI')
+        stripes = '---------------------------------------------------------------------------------------------'
+        print(heading, heading)
+        print(stripes, stripes)
+        
+        lhs = section[0]
+        rhs = section[1]   
             
-            if(bestPerforming[0] == interval or bestPerforming[1] == interval):
+        for i in range(10):
+            
+            if(section == bestPerforming[0]):
+                displayL = fmt.format(lhs[i].no, lhs[i].name, lhs[i].price, lhs[i].change1h, lhs[i].marketcap, lhs[i].roi)
+                displayR = fmt.format(rhs[i].no, rhs[i].name, rhs[i].price, rhs[i].change1h, rhs[i].marketcap, rhs[i].roi)
+          
+            if(section == bestPerforming[1]):
+                displayL = fmt.format(lhs[i].no, lhs[i].name, lhs[i].price, lhs[i].change24h, lhs[i].marketcap, lhs[i].roi)
+                displayR = fmt.format(rhs[i].no, rhs[i].name, rhs[i].price, rhs[i].change24h, rhs[i].marketcap, rhs[i].roi)
                 
-                print(fmt.format(coin.no, coin.name, coin.price, coin.change1h, coin.marketcap, coin.roi))
-            if(bestPerforming[2] == interval or bestPerforming[3] == interval):
-                
-                print(fmt.format(coin.no, coin.name, coin.price, coin.change24h, coin.marketcap, coin.roi))
-            if(bestPerforming[4] == interval or bestPerforming[5] == interval):
+            if(section == bestPerforming[2]):
+                displayL = fmt.format(lhs[i].no, lhs[i].name, lhs[i].price, lhs[i].change7d, lhs[i].marketcap, lhs[i].roi)
+                displayR = fmt.format(rhs[i].no, rhs[i].name, rhs[i].price, rhs[i].change7d, rhs[i].marketcap, rhs[i].roi)
+            print(displayL, displayR)
+        print(stripes, stripes)
+    
+    wallet_now = sorted(wallet_now, key=operator.attrgetter('no'))
 
-                print(fmt.format(coin.no, coin.name, coin.price, coin.change7d, coin.marketcap, coin.roi))
-        print()
+    print(stripes, stripes)
+    print()
+    print('**********TOP PICKS**********')
+    print(fmt.format('NO','COIN', 'PRICE', 'CHANGE', 'MARKETCAP', 'ROI'))
+    print(stripes, stripes)
+
+    for coin in wallet_now:
+        print(fmt.format(coin.no, coin.name, coin.price, coin.change7d, coin.marketcap, coin.roi))  
